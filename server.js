f67521735b14da81ccad5a83c2c0027156a0ef17
aviedb <aviedbachmid.ab@gmail.com>
2018-01-06 09:47:49 +0800

    Initial commmit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+
+var app = express()
+
+hbs.registerPartials(`${__dirname}/views/partials`)
+app.set('view engine', 'hbs')
+app.use(express.static(`${__dirname}/public`))
+
+app.use((req, res, next) => {
+    var now = new Date().toString()
+    fs.appendFile('server.log', `${now}\n`, (err) => {
+        if(err){
+            console.log('Unable to log to server.log')
+        }
+    })
+    console.log(now)
+    next()
+})
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs')
+})
+
+hbs.registerHelper('getYear', () => {
+    return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (txt) => {
+    return txt.toUpperCase()
+})
+
+app.get('/', (req, res) => {
+    //res.send('hello world!\ngo to /help.html')
+    res.render('home.hbs', {
+        welcome: 'Hello World!'
+    })
+})
+
+app.get('/bad', (req, res) => {
+    res.send('Unable to fulfill request')
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
